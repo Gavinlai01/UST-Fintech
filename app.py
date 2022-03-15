f14ef1e86e85c2c034828273bcbd0491e93c5b58
gavinlai <laichikwan01@gmail.com>
2022-03-15 14:26:04 +0800

    Add Crypto

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
 import requests
+import joblib
+import pandas as pd
+import requests
+import talib
+import json
 app = Flask(__name__)
 
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAPYVWgEAAAAAidkkoJ72QjFjmNHwoWHb5rtwagE%3DVjHzdiYMqPcEMuCTWmURKcvf2lardEydnKd34QZMrMHtBFcy51"
@@ -46,7 +51,64 @@ def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
+app = Flask(__name__)
+
+def GetData():
+    url = 'https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=49&toTs=-1&api_key=375a206b8c4f0033b8e53cd69e94931844c8f621775dcfd04740d9bfeb3e0e59'
+    resp = requests.get(url=url).json()
+    data = pd.DataFrame.from_dict(resp["Data"]["Data"])
+    data['datetime'] = pd.to_datetime(data['time'], unit='s')
+    data.set_index('datetime', inplace=True)
+    data = data.sort_values(by="datetime")
+    data = data.drop(['conversionType','conversionSymbol'], 1)
+    data['SMA20'] = talib.SMA(data['close'], timeperiod=20)
+    data['SMA50'] = talib.SMA(data['close'], timeperiod=50)
+    data['EMA20'] = talib.EMA(data['close'], timeperiod=20)
+    data['EMA50'] = talib.EMA(data['close'], timeperiod=50)
+    data['ADX'] = talib.ADX(data['high'], data['low'], data['close'], timeperiod=14)
+    data['MACD'], data['Signal'], data['Hist'] = talib.MACD(data['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+    data['RSI'] = talib.RSI(data['close'], timeperiod=14)
+    data['Upper'], data['Mid'], data['Lower'] = talib.BBANDS(data['close'], nbdevup=2, nbdevdn=2, timeperiod=20)
+    data['K'], data['D'] = talib.STOCH(data['high'], data['low'], data['close'].values, fastk_period=9, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+    data['K'].fillna(0,inplace=True)
+    data['D'].fillna(0,inplace=True)
+    data['J']=3*data['K']-2*data['D']
+    data['Willam R'] = talib.WILLR(data['high'], data['low'], data['close'], timeperiod=14)
+    data['SAR'] = talib.SAR(data['close'], data['low'], acceleration=0, maximum=0)
+    data = data.dropna()
+    return data
+
+def TreeModel(Data_np):
+    tree_clf = joblib.load('model/Tree_classifier.pkl')
+    Prediction = pd.DataFrame(tree_clf.predict(Data_np[:, 13:22]), columns = ['decision'])
+    return Prediction
+
+def ResultProcessing(Prediction,Data_np):
+    Prediction['time'] = pd.DataFrame(Data_np[:,0:1], columns = ['time'])
+    Prediction['close'] = pd.DataFrame(Data_np[:,7:8], columns = ['close'])
+    Prediction['datetime'] = pd.to_datetime(Prediction['time'], unit='s')
+    Prediction = Prediction.sort_values(by="datetime")
+    def Tostr(row):
+        if row['decision'] == 1:
+            return 'buy' 
+        elif row['decision'] == -1:
+            return 'Sell'
+        elif row['decision'] == 0:
+            return 'No decision'
+    Prediction['decision'] = Prediction.apply(lambda row: Tostr(row), axis=1)
+    result = Prediction.astype(str).to_json(orient="records")
+    return json.loads(result)
+
 @app.route('/')
+def main():
+    Data = GetData()
+    Data_np = Data.to_numpy()
+    Prediction = TreeModel(Data_np)
+    json_reulst = ResultProcessing(Prediction, Data_np)
+    print(json_reulst)
+    return render_template('crypto.html', data=json_reulst)
+
+@app.route('/finbert')
 def home():
     if request.args.get('sentence'):
         text = request.args.get('sentence')
