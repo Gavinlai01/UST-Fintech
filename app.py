c5e1819e0614f2ef3f4b4de77cf7535bd1134cdf
gavinlai <laichikwan01@gmail.com>
2022-03-18 14:40:33 +0800

    commit

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ import pandas as pd
 import requests
 import talib
 import json
+
 app = Flask(__name__)
 
+tree_clf = joblib.load('model/Tree_classifier.pkl')
+MLP_clf = joblib.load('model/MLP_classifier.pkl')
+Logistic_clf = joblib.load('model/Logistic_classifier.pkl')
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAPYVWgEAAAAAidkkoJ72QjFjmNHwoWHb5rtwagE%3DVjHzdiYMqPcEMuCTWmURKcvf2lardEydnKd34QZMrMHtBFcy51"
 
 
@@ -54,40 +58,64 @@ def query(payload):
 app = Flask(__name__)
 
 def GetData():
-    url = 'https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=49&toTs=-1&api_key=375a206b8c4f0033b8e53cd69e94931844c8f621775dcfd04740d9bfeb3e0e59'
+    url = 'https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=203&toTs=-1&api_key=375a206b8c4f0033b8e53cd69e94931844c8f621775dcfd04740d9bfeb3e0e59'
     resp = requests.get(url=url).json()
     data = pd.DataFrame.from_dict(resp["Data"]["Data"])
     data['datetime'] = pd.to_datetime(data['time'], unit='s')
     data.set_index('datetime', inplace=True)
     data = data.sort_values(by="datetime")
     data = data.drop(['conversionType','conversionSymbol'], 1)
-    data['SMA20'] = talib.SMA(data['close'], timeperiod=20)
-    data['SMA50'] = talib.SMA(data['close'], timeperiod=50)
-    data['EMA20'] = talib.EMA(data['close'], timeperiod=20)
-    data['EMA50'] = talib.EMA(data['close'], timeperiod=50)
-    data['ADX'] = talib.ADX(data['high'], data['low'], data['close'], timeperiod=14)
-    data['MACD'], data['Signal'], data['Hist'] = talib.MACD(data['close'], fastperiod=12, slowperiod=26, signalperiod=9)
-    data['RSI'] = talib.RSI(data['close'], timeperiod=14)
-    data['Upper'], data['Mid'], data['Lower'] = talib.BBANDS(data['close'], nbdevup=2, nbdevdn=2, timeperiod=20)
-    data['K'], data['D'] = talib.STOCH(data['high'], data['low'], data['close'].values, fastk_period=9, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+    data['SMA20'] = talib.SMA(data['close'], timeperiod=20) #overlap
+    data['SMA50'] = talib.SMA(data['close'], timeperiod=50) #overlap
+    data['EMA20'] = talib.EMA(data['close'], timeperiod=20) #overlap
+    data['EMA50'] = talib.EMA(data['close'], timeperiod=50) #overlap
+    data['EMA200'] = talib.EMA(data['close'], timeperiod=200) #overlap
+    data['Upper'], data['Mid'], data['Lower'] = talib.BBANDS(data['close'], nbdevup=2, nbdevdn=2, timeperiod=20) #overlap
+    data['SAR'] = talib.SAR(data['close'], data['low'], acceleration=0, maximum=0) #overlap
+    data['ADX'] = talib.ADX(data['high'], data['low'], data['close'], timeperiod=14) #Momentum
+    data['MACD'], data['Signal'], data['Hist'] = talib.MACD(data['close'], fastperiod=12, slowperiod=26, signalperiod=9) #Momentum
+    data['RSI'] = talib.RSI(data['close'], timeperiod=14) #Momentum
+    data['K'], data['D'] = talib.STOCH(data['high'], data['low'], data['close'].values, fastk_period=9, slowk_period=3, #Momentum
+                                slowk_matype=0, slowd_period=3, slowd_matype=0)
     data['K'].fillna(0,inplace=True)
     data['D'].fillna(0,inplace=True)
     data['J']=3*data['K']-2*data['D']
-    data['Willam R'] = talib.WILLR(data['high'], data['low'], data['close'], timeperiod=14)
-    data['SAR'] = talib.SAR(data['close'], data['low'], acceleration=0, maximum=0)
+    data['Willam R'] = talib.WILLR(data['high'], data['low'], data['close'], timeperiod=14)  #Momentum
+    data['slowk'], data['slowd'] = talib.STOCH(data['high'], data['low'],data['close']) #Momentum
+    data['ADOSC'] = talib.ADOSC(data['high'], data['low'], data['close'], data['volumeto']) #Momentum
+    data['MFI'] = talib.MFI(data['high'], data['low'], data['close'], data['volumeto']) #Momentum
+    data['aroondown'], data['aroonup'] = talib.AROON(data['close'], data['low']) #Volume
     data = data.dropna()
     return data
 
-def TreeModel(Data_np):
-    tree_clf = joblib.load('model/Tree_classifier.pkl')
-    Prediction = pd.DataFrame(tree_clf.predict(Data_np[:, 13:22]), columns = ['decision'])
+def TreeModel(Data):
+    Data_np = Data.to_numpy()
+    Prediction = pd.DataFrame(tree_clf.predict(Data_np[:, 7:]), columns = ['decision'])
     return Prediction
 
-def ResultProcessing(Prediction,Data_np):
+def MLPModel(Data):
+    Data_np = Data.to_numpy()
+    Prediction = pd.DataFrame(MLP_clf.predict(Data_np[:, 13:22]), columns = ['decision'])
+    return Prediction
+
+def LogisticModel(Data):
+    Data_np = Data.to_numpy()
+    Prediction = pd.DataFrame(Logistic_clf.predict(Data_np[:, 7:]), columns = ['decision'])
+    return Prediction
+
+def NBModel(Data):
+    Data_np = Data.to_numpy()
+    Prediction = pd.DataFrame(Logistic_clf.predict(Data_np[:, 7:]), columns = ['decision'])
+    return Prediction
+
+def ResultProcessing(Prediction,Data_np, Data):
     Prediction['time'] = pd.DataFrame(Data_np[:,0:1], columns = ['time'])
     Prediction['close'] = pd.DataFrame(Data_np[:,7:8], columns = ['close'])
     Prediction['datetime'] = pd.to_datetime(Prediction['time'], unit='s')
+    Prediction['Datetime'] = pd.to_datetime(Prediction['time'], unit='s')
     Prediction = Prediction.sort_values(by="datetime")
+    Prediction.set_index('datetime', inplace=True)
+    Prediction['close'] = Data['close']
     def Tostr(row):
         if row['decision'] == 1:
             return 'buy' 
@@ -103,10 +131,33 @@ def ResultProcessing(Prediction,Data_np):
 def main():
     Data = GetData()
     Data_np = Data.to_numpy()
-    Prediction = TreeModel(Data_np)
-    json_reulst = ResultProcessing(Prediction, Data_np)
-    print(json_reulst)
-    return render_template('crypto.html', data=json_reulst)
+    Prediction = TreeModel(Data)
+    json_reulst = ResultProcessing(Prediction, Data_np, Data)
+    return render_template('tree.html', data=json_reulst)
+
+@app.route('/mlp')
+def MLP():
+    Data = GetData()
+    Data_np = Data.to_numpy()
+    Prediction = MLPModel(Data)
+    json_reulst = ResultProcessing(Prediction, Data_np, Data)
+    return render_template('mlp.html', data=json_reulst)
+
+@app.route('/logistic')
+def Logistic():
+    Data = GetData()
+    Data_np = Data.to_numpy()
+    Prediction = LogisticModel(Data)
+    json_reulst = ResultProcessing(Prediction, Data_np, Data)
+    return render_template('logistic.html', data=json_reulst)
+
+@app.route('/naive-bayes')
+def KNN():
+    Data = GetData()
+    Data_np = Data.to_numpy()
+    Prediction = NBModel(Data)
+    json_reulst = ResultProcessing(Prediction, Data_np, Data)
+    return render_template('nb.html', data=json_reulst)
 
 @app.route('/finbert')
 def home():
